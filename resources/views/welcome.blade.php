891de579b8a43cf6d8c2240865d797e424ea2735
Mehedi Hasan <46008075+mehedi7253@users.noreply.github.com>
2022-01-27 18:45:40 +0600

    add logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="{{ asset('assets/style/main.css') }}" type="text/css">
     <link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@300;700&display=swap" rel="stylesheet">
+    <link rel="icon" href="{{ asset('images/logo/spacetech-logo.png') }}">
 </head>
 <body>
     <section class="top_header">
@@ -17,7 +18,7 @@
                 <div class="col-md-10 col-sm-12 float-left top_header_link">
                     <i class="fa fa-phone"></i><a href="tel:01730711050" class="text-decoration-none"> (+88) 01730 711050</a> |
                     <i class="fa fa-envelope"></i><a href="mailto:corporateask@gmail.com?subject = Feedback&body = Message" class="text-decoration-none"> spacetechelevators@gmail.com</a> |
-                    <i class="fa fa-clock-o"> 247/7 Hours Working</i>
+                    <i class="fa fa-clock-o"> 24/7 Support</i>
                 </div>
                 <div class="col-md-2 col-sm-12 float-left top_social_icon">
                     <ul>
