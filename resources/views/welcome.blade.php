b3d36bc634a93ac2edc74228b9b27e04bd65f221
Mehedi Hasan <46008075+mehedi7253@users.noreply.github.com>
2022-02-01 17:01:49 +0600

    added about us and project section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,29 +84,44 @@
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
           <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-          {{-- <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li> --}}
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
         </ol>
         <div class="carousel-inner">
           <div class="carousel-item active">
             <img class="d-block w-100" src="{{ asset('images/banner/Banner_1.jpg') }}" alt="First slide">
-            <div class="col-md-7 mx-auto carousel-caption" style="border: 1px solid red">
-                <h2 class="text-white font-weight-bold font-italic" style="font-size: 70px; font-weight: 700; text-shadow: 5px 5px 3px #000;"><span style="color:#0172BC;">SpaceTech</span> <br/>
-                    <span style="color: #FF0202;">Elevator & Escalator</span>
+            <div class="col-md-12 col-sm-12 carousel_caption">
+                <h2 class="banner_text"><span style="color:#FF0202;">SPACE<span style="color: #0172BC">TECH</span></span> <br/>
+                    <span style="color: white;">Elevator & Escalator</span>
                 </h2>
             </div>
         </div>
-          {{-- <div class="carousel-item">
+          <div class="carousel-item">
             <img class="d-block w-100" src="{{ asset('images/banner/elevator-engine-elevator-engine-abandoned-house-118425485.jpg') }}" alt="Second slide" >
-          </div>
+            <div class="col-md-12 col-sm-12 carousel_caption">
+                <h2 class=" banner_text"><span style="color:#FF0202;">SPACE<span style="color: #0172BC">TECH</span></span> <br/>
+                    <span style="color: white;">Elevator & Escalator</span>
+                </h2>
+            </div>
+        </div>
           <div class="carousel-item">
             <img class="d-block w-100" src="{{ asset('images/banner/Maintenance-Banner.jpg') }}" alt="Third slide" >
-          </div>
+            <div class="col-md-12 col-sm-12 carousel_caption">
+                <h2 class=" banner_text"><span style="color:#FF0202;">SPACE<span style="color: #0172BC">TECH</span></span> <br/>
+                    <span style="color: white;">Elevator & Escalator</span>
+                </h2>
+            </div>
+        </div>
           <div class="carousel-item">
             <img class="d-block w-100" src="{{ asset('images/banner/istockphoto-1045852986-612x612.jpg') }}" alt="Fourth slide" >
-          </div>
-        </div> --}}
+            <div class="col-md-12 col-sm-12 carousel_caption">
+                <h2 class=" banner_text"><span style="color:#FF0202;">SPACE<span style="color: #0172BC">TECH</span></span> <br/>
+                    <span style="color: white;">Elevator & Escalator</span>
+                </h2>
+            </div>
+        </div>
+        </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="sr-only">Previous</span>
@@ -115,52 +130,101 @@
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
         </a>
+    </div>
+    {{-- end banner --}}
 
+    {{-- about us section --}}
+    <section class="about_us">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7 col-sm-12 float-left mt-5 mb-5" style="border-right: 2px dotted #0172BC;">
+                    <h3 class="font-weight-bold about_heading"><span style="color: #FF0202">About</span><span style="color: #0172BC">Us</span></h3>
+                    <p class="text-justify">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae hic quam debitis architecto doloribus dolor impedit? Fugit, esse! Officiis dolor ut, soluta architecto esse delectus praesentium expedita culpa facilis quis!
+                        <br/> <br/>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae hic quam debitis architecto doloribus dolor impedit? Fugit, esse! Officiis dolor ut, soluta architecto esse delectus praesentium expedita culpa facilis quis!
+                    </p>
+                </div>
+                <div class="col-md-5 col-sm-12 mt-5 mb-5 float-left">
+                    <h3 class="font-weight-bold service_provide" style="color: #FF0202" >Service We<span style="color: #0172BC"> Provide</span> </h3>
 
-        {{-- <section class="slider_contatct" style="margin-top: -35%;">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 col-sm-12 mb-5 mt-5 float-left">
-                        <h2 class="text-white font-weight-bold font-italic d-none d-lg-block" style="font-size: 70px; font-weight: 700; text-shadow: 5px 5px 3px #000; margin-top:100px"><span style="color:#0172BC;">SpaceTech</span> <br/>
-                            <span style="margin-left: 42px; color: #FF0202;">Elevator & Escalator</span>
-                        </h2>
+                    <div class="ml-3">
+                        <li class="fa fa-arrow-circle-right" style="color: #0172BC"> Passenger Lift</li><br/>
+                        <li class="fa fa-arrow-circle-right " style="color: #FF0202"> Escalator</li><br/>
+                        <li class="fa fa-arrow-circle-right " style="color: #0172BC"> Cargo Lift</li><br/>
+                        <li class="fa fa-arrow-circle-right " style="color: #FF0202"> Hospital Lift</li><br/>
+                        <li class="fa fa-arrow-circle-right " style="color: #0172BC"> Capsul Lift</li><br/>
+                        <li class="fa fa-arrow-circle-right " style="color: #FF0202"> Observation Lift</li><br/>
+                        <li class="fa fa-arrow-circle-right " style="color: #0172BC"> Car Lift</li><br/>
+                        <li class="fa fa-arrow-circle-right " style="color: #FF0202"> Bed Lift</li><br/>
                     </div>
-                    <div class="col-md-4 col-sm-12 mb-5 mt-5 float-left">
-                        <div class="card" style="border: 1px solid #FF0202">
-                            <div class="card-header" style="background-color: #FF0202">
-                                <p class="text-center"><span class="font-weight-bolder text-capitalize">Send Message</span> </p>
-                            </div>
-                            <div class="card-body" style="background-color: #0172BC">
-                               <form action="" method="post">
-                                    <div class="form-group">
-                                        <input type="text" name="name" placeholder="Name.." class="form-control">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="number" name="phone" placeholder="Phone Number.." class="form-control">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="email" name="email" placeholder="Email..." class="form-control">
-                                    </div>
-                                    <div class="form-group">
-                                        <textarea placeholder="Message...." class="form-control" name="message"></textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="submit" name="btn" class="btn btn-success" value="Send Message">
-                                    </div>
-                                </form>
-                            </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- about us section --}}
+
+    {{-- working demo --}}
+    <section class="showcase">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 mb-5 mt-5">
+                    <h3 class="text-center text-danger mb-5">Our Previous Work</h3>
+                    <div align="center">
+                        <button class="btn btn-default filter-button" data-filter="all">All</button>
+                        <button class="btn btn-default filter-button" data-filter="ongoing">Ongoing</button>
+                        <button class="btn btn-default filter-button" data-filter="complete">Complete</button>
+                    </div>
+                    <br />
+
+                    <div class="col-md-3 col-sm-12 float-left filter ongoing mt-3">
+                        <div class="thumbnail">
+                            <img alt="" src="{{ asset('images/project/istockphoto-1025375470-170667a.jpg') }}" class="filter_image">
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-12 float-left ongoing filter mt-3">
+                        <div class="thumbnail">
+                            <img alt="" src="{{ asset('images/project/istockphoto-1025375470-170667a.jpg') }}" class="filter_image">
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-12 float-left ongoing filter mt-3">
+                        <div class="thumbnail">
+                            <img alt="" src="{{ asset('images/project/istockphoto-1025375470-170667a.jpg') }}" class="filter_image">
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-12 float-left ongoing filter mt-3">
+                        <div class="thumbnail">
+                            <img alt="" src="{{ asset('images/project/istockphoto-1025375470-170667a.jpg') }}" class="filter_image">
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-12 float-left complete filter mt-3" >
+                        <div class="thumbnail">
+                            <img alt="" src="{{ asset('images/project/istockphoto-1025375470-170667a.jpg') }}" class="filter_image" >
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-12 float-left complete filter mt-3" >
+                        <div class="thumbnail">
+                            <img alt="" src="{{ asset('images/project/istockphoto-1025375470-170667a.jpg') }}" class="filter_image">
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-12 float-left complete filter mt-3" >
+                        <div class="thumbnail">
+                            <img alt="" src="{{ asset('images/project/istockphoto-1025375470-170667a.jpg') }}" class="filter_image">
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-12 float-left complete filter mt-3" >
+                        <div class="thumbnail">
+                            <img alt="" src="{{ asset('images/project/istockphoto-1025375470-170667a.jpg') }}" class="filter_image">
                         </div>
                     </div>
                 </div>
             </div>
-        </section> --}}
-
-      </div>
-
-      {{-- end banner --}}
+        </div>
+    </section>
+    {{-- working demo --}}
     <script src="{{ asset('assets/js/jquery.js') }}"></script>
     <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('assets/js/bootstrap.bundle.js') }}"></script>
+    <script src="{{ asset('assets/js/filter.js') }}"></script>
     <script>
          $(document).on("scroll", function() {
             if ($(document).scrollTop() > 86) {
