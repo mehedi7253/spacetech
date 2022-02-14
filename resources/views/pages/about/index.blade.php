1c89a32b4a702bb3824db1ad2a3a78be1785ff34
Mehedi Hasan <46008075+mehedi7253@users.noreply.github.com>
2022-02-14 18:23:28 +0600

    delete js file and some blank image

--- resources/views/pages/about/index.blade.php
+++ resources/views/pages/about/index.blade.php
@@ -7,7 +7,9 @@
                 </div>
                 <div class="col-md-8 col-sm-12 float-left">
                     <p class="text-justify">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus officia vero ducimus doloremque, debitis dolorem. Commodi impedit mollitia temporibus labore voluptates totam beatae natus cum perferendis vel? Dolore, esse doloremque.
+                        SPACETECH ELEVATORS being one of the reputable global leaders in elevator & escalator industry has successfully introduced the concept of combining advanced lift technology from Europe with Standard & Luxury design elements, achieving a high standard performance at a competitive price. As per quality SPACE TECH works absolutely like Brand Elevator.
+                        <br/><br/>
+                        World class elevator component technologies employed by SPACETECH over the years have proven their compatibility, in particular with regards to size, speed, power efficiency, proper installation and noiseless performance. With safety features second to none in the industry, these technical liaisons have put SPACETECH in position to offer local developers and architects new elevator design such as the "Gear-Less Concept". which allows a better utilization of commercial building space and thus better return on investment.
                     </p>
                 </div>
                 <div class="col-md-4 col-sm-12 float-left mb-5">
